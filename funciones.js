4e1254803736b604f4f12831bd3cac0736356914
David19T <122892539+David19T@users.noreply.github.com>
2023-02-28 21:44:04 -0300

    Add files via upload
    Examen de Ticmas - Programacion Front-End.

---
+++ funciones.js
@@ -0,0 +1,9 @@
+
+function Boton1() { /* ia1 significa informacion academica 1. */
+    var ia1 = document.getElementById("infoAcademica");
+    if (ia1.style.display === "none") {
+      ia1.style.display = "block";
+    } else {
+      ia1.style.display = "none";
+    }
+  }
